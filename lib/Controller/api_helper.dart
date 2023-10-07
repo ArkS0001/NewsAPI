c3786c0f07d0e4572c977970755d27b1da1a90c8
Aakarshit Srivastava <113760964+ArkS0001@users.noreply.github.com>
2023-10-07 13:27:23 +0530

    Update api_helper.dart

--- lib/Controller/api_helper.dart
+++ lib/Controller/api_helper.dart
@@ -4,7 +4,7 @@ import 'package:news_udemy/Model/news_model.dart';
 class NewsHelper extends GetConnect implements GetxService {
   Future<List<NewsModel>> getHeadlines() async {
     Response response = await get(
-        "https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=a9c0b03781794dfe911fa77e728ceb1a");
+        "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=a9c0b03781794dfe911fa77e728ceb1a");
     List data = response.body["articles"];
     List<NewsModel> newsModel = data.map((e) => NewsModel.fromJson(e)).toList();
     return newsModel;
@@ -12,7 +12,7 @@ class NewsHelper extends GetConnect implements GetxService {
 
   Future<List<NewsModel>> getNewsCategory({required String category}) async {
     Response response = await get(
-        "https://newsapi.org/v2/top-headlines?country=us&category=$category&apiKey=a9c0b03781794dfe911fa77e728ceb1a");
+        "https://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=a9c0b03781794dfe911fa77e728ceb1a");
     List data = response.body["articles"];
     List<NewsModel> newsModel = data.map((e) => NewsModel.fromJson(e)).toList();
     return newsModel;
